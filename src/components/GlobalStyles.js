93685050e08291fe5f104b404c0ce61720b4b300
mhso-dev <mhso.developer@gmail.com>
2020-12-03 10:53:36 +0900

    background 복구

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -13,9 +13,9 @@ const globalStyles = createGlobalStyle`
     body{
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size:12px;
-        background-color: "#98acf8";
+        background-color:rgba(20,20,20,1);
         color:white;
-        padding-top:50px;
+        padding-top:50px
     }
 `;
 
